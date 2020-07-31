47088c29c4d1c341887453e32b854c5ac4ed468b
zhangheng <1585026544@qq.com>
2020-07-31 22:24:44 +0800

    '增加链表操作中对nul的判断'

--- src/com/leetcode/utils/List.java
+++ src/com/leetcode/utils/List.java
@@ -4,6 +4,10 @@ import com.leetcode.datastructure.ListNode;
 
 public class List {
 	public static ListNode generateList(int count) {
+		if(count == 0) {
+			System.out.println("链表长度不能为0");
+			return null;
+		}
 		ListNode head = new ListNode(1);
 		ListNode currNode = head;
 		for(int i = 2; i <= count ; i++) {
@@ -15,6 +19,9 @@ public class List {
 	}
 	
 	public static void printList(ListNode head) {
+		if(head == null) {
+			System.out.println("链表为空！");
+		}
 		while(head != null) {
 			if(head.next != null) {
 				System.out.print(head.val + "->");
