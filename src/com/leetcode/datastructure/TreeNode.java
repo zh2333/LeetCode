37dc2d6a31ce4a7024c3401f5482c0837ac00b8a
zhangheng <1585026544@qq.com>
2020-08-03 23:10:52 +0800

    '新增TreeNode的构造方法'

--- src/com/leetcode/datastructure/TreeNode.java
+++ src/com/leetcode/datastructure/TreeNode.java
@@ -4,5 +4,10 @@ public class TreeNode {
 	 public int val;
 	 public TreeNode left;
 	 public TreeNode right;
-	 public TreeNode(int x) { val = x; }
+	 public TreeNode(int x) { this.val = x; }
+	 TreeNode(int val, TreeNode left, TreeNode right) {
+		this.val = val;
+		this.left = left;
+		this.right = right;
+     }
 }
