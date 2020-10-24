2f49d731deb5a9c136f3be3b63272571255f17e7
zhttyzhang <zhttyzhang@tencent.com>
2020-10-24 15:13:48 +0800

    MyArray新增辅助测试方法, 将LeetCode官方提供的数组样式的字符串测试样例转换为数组, 暂时只支持int[][]

--- src/com/leetcode/utils/MyArray.java
+++ src/com/leetcode/utils/MyArray.java
@@ -2,6 +2,8 @@ package com.leetcode.utils;
 
 import org.junit.Test;
 
+import java.util.ArrayList;
+
 /**
  * �������Թ�����, ��������ĳ��ò���
  * @author �ź�
@@ -83,6 +85,42 @@ public class MyArray {
 		int tmp = Array[index1];
 		Array[index1] = Array[index2];
 		Array[index2] = tmp;
+	}
+
+	/**
+	 * 将官方给的字符串测试用例转换成数组
+	 */
+	public static int[][] convertStrToArray(String str) {
+		int arrows = 0;
+		String sub = str.substring(1, str.length() - 1);
+		int len = sub.length();
+		ArrayList<String[]> res = new ArrayList<>();
+		int i = 0;
+		int j = 0;
+		while (i < len) {
+			String tmpStr = "";
+			if (sub.charAt(i) == '[') {
+				j = i;
+				while (sub.charAt(j) != ']') {
+					j++;
+				}
+			} else {
+				i++;
+				continue;
+			}
+			tmpStr = sub.substring(i + 1, j);
+			String[] arr = tmpStr.split(",");
+			res.add(arr);
+			arrows++;
+			i = j;
+		}
+		int[][] twoDimArray = new int[arrows][2];
+		for (int k = 0; k < arrows; k++) {
+			String[] arr = res.get(k);
+			twoDimArray[k][0] = Integer.valueOf(arr[0]);
+			twoDimArray[k][1] = Integer.valueOf(arr[1]);
+		}
+		return twoDimArray;
 	}
 	
 }
